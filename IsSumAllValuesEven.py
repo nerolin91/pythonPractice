d8841d9228f40048ffa5feac72d1a4a2abe34674
nerol <nerol@sfu.ca>
2016-08-02 21:18:35 -0700

    isSum done

---
+++ IsSumAllValuesEven.py
@@ -0,0 +1,14 @@
+def isSumAllValuesEven(mat):
+    sum=0
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            sum+=mat[i][j]
+    print sum
+    if sum%2==0:
+        return True
+    else:
+        return False
+mat=[[1,1,1],[2,2,2]]
+res = isSumAllValuesEven(mat)
+print (res) 
+
